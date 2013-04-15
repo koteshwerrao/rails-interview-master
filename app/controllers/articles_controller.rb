1afc3999895651f33a4d389c9ec01d0f41ad1674
srinivasa <srinivasa@heurion.com>
2013-04-15 19:59:05 +0530

    changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,13 @@
 class ArticlesController < ApplicationController
 
   def index
-    unless params[:page].blank?
+   # unless params[:page].blank?
       feed = Feedzirra::Feed.fetch_and_parse("http://feeds.wired.com/wired/index?format=rss") 
       feed.entries.each do |art|
         article=Article.where(:title=>art.title).first
         article=Article.create(:published=>art.published,:author=>art.author,:content=>art.content,:url=>art.url,:title=>art.title) if article.nil?
       end
-    end
+   # end
     @articles=Article.order("created_at desc").paginate(:page => params[:page], :per_page => 10)
   end
 
